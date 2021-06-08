9f67dd4d69cb536cbce5bdaa88a57ca3dd131569
Marco Antonio Santana <marco@santana.nyc>
2021-06-08 19:10:02 +0000

    oops I missed the screenlayout

---
+++ screenlayout/displaywall.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 3558x2160 --rotate left --output HDMI-1 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-3 --mode 1920x1080 --pos 4638x2160 --rotate right --output HDMI-1-0 --off --output HDMI-1-1 --off --output HDMI-1-2 --off --output HDMI-1-3 --off
