8ca22958861ed93e2b832871b9ea2e7ca73a97b1
Marco Antonio Santana <marco@santana.nyc>
2021-06-22 15:56:47 -0400

    a little more organizing and fixing some order of operations

--- scripts/labcam.sh
+++ scripts/labcam.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-ffplay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp"
+echo "do nothing for now!"
+#ffplay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp"
 
 #vlc -f rtsp://displaywall:display@192.168.1.40/axis-media/media.amp
