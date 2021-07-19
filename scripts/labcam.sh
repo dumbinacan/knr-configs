aa63d0f2ee38bed4bcfcd03e563a36d86e4605c9
Marco Antonio Santana <marco@santana.nyc>
2021-07-19 15:11:43 -0400

    added stream profile to lab cam stream

--- scripts/labcam.sh
+++ scripts/labcam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp"
+ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp&streamprofile=display_wall"
 
 #vlc -f rtsp://displaywall:display@192.168.1.40/axis-media/media.amp
