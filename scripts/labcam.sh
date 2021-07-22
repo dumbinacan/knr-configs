cf2a54d4f2995d6236f32ce1b189389d62407ebd
Marco Antonio Santana <marco@santana.nyc>
2021-07-22 16:38:31 -0400

    going mjpg to reduce delay

--- scripts/labcam.sh
+++ scripts/labcam.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+ffplay http://displaywall:display@192.168.1.40/mjpg/video.mjpg 
 #ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp?streamprofile=display_wall"
 #ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp?streamprofile=display_wall"
 
-vlc -f rtsp://displaywall:display@192.168.1.40/axis-media/media.amp --rtsp-http
+#vlc -f rtsp://displaywall:display@192.168.1.40/axis-media/media.amp --rtsp-http
