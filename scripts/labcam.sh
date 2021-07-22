5f62ed1a58b4865043f8c5f9e411b848529d6aeb
Marco Antonio Santana <marco@santana.nyc>
2021-07-22 10:58:27 -0400

    test vlc config I found online

--- scripts/labcam.sh
+++ scripts/labcam.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp?streamprofile=display_wall"
+#ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp?streamprofile=display_wall"
+#ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp?streamprofile=display_wall"
 
-#vlc -f rtsp://displaywall:display@192.168.1.40/axis-media/media.amp
+vlc -f rtsp://displaywall:display@192.168.1.40/axis-media/media.amp --rtsp-http
