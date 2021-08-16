6b705102f2eefb3c9893875241239ee00937d380
Marco Antonio Santana <marco@santana.nyc>
2021-08-16 10:31:46 -0400

    added lower res stream profile

--- scripts/labcam.sh
+++ scripts/labcam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ffplay -fflags nobuffer -flags low_delay http://displaywall:display@192.168.1.40/mjpg/video.mjpg 
+ffplay -fflags nobuffer -flags low_delay http://displaywall:display@192.168.1.40/mjpg/video.mjpg?streamprofile=display_wall_mjpg
 #ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp?streamprofile=display_wall"
 #ffplay -fflags nobuffer -flags low_delay -rtsp_transport tcp -i "rtsp://displaywall:display@192.168.1.40/axis-media/media.amp?streamprofile=display_wall"
 
