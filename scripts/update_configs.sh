7234f631cf3ac779f35deced0db8d4b9211828b5
Marco Antonio Santana <marco@santana.nyc>
2021-09-10 12:19:16 -0400

    update formatting for logging updates

--- scripts/update_configs.sh
+++ scripts/update_configs.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 cd ~/knr-configs
-echo "## $(date) ##" >> ./update.log
+echo "## $(date) ##\ngit pull" >> ./update.log
 git pull >> ./update.log
+echo "# pacman -Syyu --noconfirm" >> ./update.log
 sudo pacman -Syyu --noconfirm >> ./update.log && wait;
+echo "##################################\n" >> ./update.log
 git add ./update.log
 git commit -m "updates from $(date)"
 git push
