8bff892ca92d8f4786a6a8f151715ca6e66fbcb2
Marco Antonio Santana <marco@santana.nyc>
2021-09-10 07:49:41 -0700

    push logs from updates back to git

--- scripts/update_configs.sh
+++ scripts/update_configs.sh
@@ -4,3 +4,6 @@ cd ~/knr-configs
 echo "## $(date) ##" >> ./update.log
 git pull >> ./update.log
 sudo pacman -Syyu --noconfirm >> ./update.log && wait;
+git add ./update.log
+git commit -m "updates from $(date)"
+git push
