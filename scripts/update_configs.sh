d77be5435fd85bbefc5a71284f410e122a6b850e
Marco Antonio Santana <marco@santana.nyc>
2021-09-16 10:54:24 -0400

    formatting fixes

--- scripts/update_configs.sh
+++ scripts/update_configs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd ~/knr-configs
-printf "## $(date) ##\ngit pull\n" >> ./update.log
+printf "## $(date) ##\n\$ git pull\n" >> ./update.log
 git pull >> ./update.log
 printf "# pacman -Syyu --noconfirm\n" >> ./update.log
 sudo pacman -Syyu --noconfirm >> ./update.log && wait;
-printf "##################################\n" >> ./update.log
+printf "#####################################\n" >> ./update.log
 git add ./update.log
 git commit -m "updates from $(date)"
 git push
