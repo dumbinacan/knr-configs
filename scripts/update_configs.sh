f102ec925262dd454c515bd3c32dc3301fdc34f1
Marco Antonio Santana <marco@santana.nyc>
2021-09-16 10:36:43 -0400

    switched to printf to use \n

--- scripts/update_configs.sh
+++ scripts/update_configs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd ~/knr-configs
-echo "## $(date) ##\ngit pull" >> ./update.log
+printf "## $(date) ##\ngit pull" >> ./update.log
 git pull >> ./update.log
-echo "# pacman -Syyu --noconfirm" >> ./update.log
+printf "# pacman -Syyu --noconfirm" >> ./update.log
 sudo pacman -Syyu --noconfirm >> ./update.log && wait;
-echo "##################################\n" >> ./update.log
+printf "##################################\n" >> ./update.log
 git add ./update.log
 git commit -m "updates from $(date)"
 git push
